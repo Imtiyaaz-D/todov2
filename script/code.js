87b801ec4808b1f5047d50c5807c574986eb75be
Imtiyaaz-D <imtdamon777@gmail.com>
2023-06-05 16:48:38 +0200

    add

--- script/code.js
+++ script/code.js
@@ -1,15 +1,25 @@
 const add = document.querySelector("#addItem")
 const sort = document.querySelector("#sortItem")
 const inputName = document.querySelector("#listInput")
-let list = document.getElementById('#itemList')
+let list = document.getElementById('itemList')
 
-let tasks = []
-function addItem() {
-    array.push(inputName.value);
+let lists = []
+// function addItem() {
+//     lists.push(inputName.value);
+//     list.innerHTML = "";
+//     lists.forEach((item) => {
+//         list.innerHTML += `
+//         <div id="checkbox"><input type="checkbox">`+item+`<button id="xButton">&times</button></div>
+//         `
+//     })
+// }
+
+add.addEventListener('click', (e)=>{
     list.innerHTML = "";
-    tasks.forEach((item) => {
+    lists.push(inputName.value)
+    lists.forEach((item) => {
         list.innerHTML += `
-        <div id="checkbox"><input type="checkbox">`+item+`<button id="xButton">&times</button></div>
+        <li id="checkbox"><input type="checkbox">`+item+`<button id="xButton">&times</button></li>
         `
     })
-}
+})
