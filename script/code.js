6d7ef78a3a0b41211d7193b1b4e484bcc1aa1899
Imtiyaaz-D <imtdamon777@gmail.com>
2023-06-05 16:32:27 +0200

    java

--- script/code.js
+++ script/code.js
@@ -1,1 +1,15 @@
-let inputBox = document.querySelector(#)
+const add = document.querySelector("#addItem")
+const sort = document.querySelector("#sortItem")
+const inputName = document.querySelector("#listInput")
+let list = document.getElementById('#itemList')
+
+let tasks = []
+function addItem() {
+    array.push(inputName.value);
+    list.innerHTML = "";
+    tasks.forEach((item) => {
+        list.innerHTML += `
+        <div id="checkbox"><input type="checkbox">`+item+`<button id="xButton">&times</button></div>
+        `
+    })
+}
